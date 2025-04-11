d51e2447a1fdd643fdf6f93f0888dde5faa2fd20
SRee1721 <143257755+SRee1721@users.noreply.github.com>
2025-04-12 01:25:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ app = Flask(__name__)
 COLLECTION_NAME = "academy:register"
 
 
-'''@app.route('/')
+@app.route('/')
 def index():
-    return render_template('index.html')'''
+    return render_template('index.html')
 
 
 @app.route('/start-face-collection', methods=['POST'])
